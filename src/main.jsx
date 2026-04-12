5f443dc5a858eeb924fd9709e7f42d06b00567bb
Kaushik S <75175772+kaushiksaravanan@users.noreply.github.com>
2026-04-12 23:39:25 +0530

    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styleGuide.css";
 import "./fonts.css";
 import "./parallel/hero-top.css";
 import "./parallel/middle-sections.css";
