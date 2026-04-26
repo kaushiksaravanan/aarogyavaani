6f429de1136f934b37785321400fca4f70817c71
Kaushik S <75175772+kaushiksaravanan@users.noreply.github.com>
2026-04-26 13:55:58 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
 @echo off
-
 start cmd /K "cd /d %~dp0 && npm install && npm run dev"
 start cmd /K "cd /d %~dp0\backend && python -m venv .venv && call .venv\Scripts\activate && pip install -r requirements.txt && python app.py"
